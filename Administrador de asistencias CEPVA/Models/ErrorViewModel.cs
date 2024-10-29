a2617a01e80e0fb2df2739c36fad4d9f7d52bf44
Maxwell Tavares <maxwelltrt0709@gmail.com>
2024-10-28 22:45:01 -0400

    Agregar archivos de proyecto.

---
+++ Administrador de asistencias CEPVA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Administrador_de_asistencias_CEPVA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
