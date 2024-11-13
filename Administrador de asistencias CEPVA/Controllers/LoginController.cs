e7c7b12f420abc8b9bbd15d8de96f3544ac529cc
ZetitaDev <EfrenDaniel2610@gmail.com>
2024-11-13 08:23:22 -0500

    Dashboard agregado

--- Administrador de asistencias CEPVA/Controllers/LoginController.cs
+++ Administrador de asistencias CEPVA/Controllers/LoginController.cs
@@ -9,5 +9,9 @@ namespace Administrador_de_asistencias_CEPVA.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
     }
 }
