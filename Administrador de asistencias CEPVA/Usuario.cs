d1146938258bbc27c6437dc951b7104976c45ec3
ZetitaDev <EfrenDaniel2610@gmail.com>
2024-11-22 23:18:19 -0500

    Conexion a la base de datos agregada correctamente

---
+++ Administrador de asistencias CEPVA/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TuProyecto.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string? Sexo { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public int? Edad { get; set; }
+        public string? Nacionalidad { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string CorreoElectronico { get; set; } = null!;
+        public string Contrasena { get; set; } = null!;
+        public byte[]? FotoPerfil { get; set; } // Para la imagen del perfil
+    }
+}
